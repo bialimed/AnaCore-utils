da57f83362a2082f12223a276429ea9350ad11e4
fescudie <escudie.frederic@iuct-oncopole.fr>
2019-11-20 18:09:59 +0100

    Fix bug with FILTER separator used on input VCF.

--- bin/filterVCFByAnnot.py
+++ bin/filterVCFByAnnot.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
-#
-# Copyright (C) 2019 IUCT-O
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2019 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -200,7 +184,7 @@ if __name__ == "__main__":
                         old_filters = set()
                         if "FILTER" in annot and annot["FILTER"] is not None:
                             if annot["FILTER"] != "PASS":
-                                old_filters = set(annot["FILTER"].split(","))
+                                old_filters = set(annot["FILTER"].split("&"))
                         annot["FILTER"] = set()
                         # Not same variant
                         tagCollocated(annot, alt)
