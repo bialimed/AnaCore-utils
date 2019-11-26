53bdd2d704a9089854ac0ea5550285e76f8e6d49
fescudie <escudie.frederic@iuct-oncopole.fr>
2019-11-26 21:45:08 +0100

    Add filter on lowDP in bin/filterVCFByAC.py.

--- bin/filterVCFByAC.py
+++ bin/filterVCFByAC.py
@@ -3,7 +3,7 @@
 __author__ = 'Frederic Escudie'
 __copyright__ = 'Copyright (C) 2019 IUCT-O'
 __license__ = 'GNU General Public License'
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 __email__ = 'escudie.frederic@iuct-oncopole.fr'
 __status__ = 'prod'
 
@@ -21,10 +21,11 @@ from anacore.vcf import VCFIO, HeaderFilterAttr
 ########################################################################
 if __name__ == "__main__":
     # Manage parameters
-    parser = argparse.ArgumentParser(description='Filter variants on their AD and AF.')
+    parser = argparse.ArgumentParser(description='Filter variants on their AD, AF and DP.')
     parser.add_argument('-m', '--mode', default="tag", choices=["tag", "remove"], help='Select the filter mode. In mode "tag" if the variant does not fit criteria a tag "lowAD" and/or "lowAF" is added in FILTER field. In mode "remove" if the variant does not fit criteria it is removed from the output. [Default: %(default)s]')
     parser.add_argument('-d', '--min-AD', default=4, type=int, help='Filter variants with AD <= than this values. [Default: %(default)s]')
     parser.add_argument('-f', '--min-AF', default=0.02, type=float, help='Filter variants with AF <= than this values. [Default: %(default)s]')
+    parser.add_argument('-p', '--min-DP', default=20, type=int, help='Filter variants with DP <= than this values. [Default: %(default)s]')
     group_input = parser.add_argument_group('Inputs')  # Inputs
     group_input.add_argument('-i', '--input-variants', help='Path to the variants file (format: VCF).')
     group_output = parser.add_argument_group('Outputs')  # Outputs
@@ -44,12 +45,12 @@ if __name__ == "__main__":
         with VCFIO(args.output_variants, "w") as FH_out:
             # Header
             FH_out.copyHeader(FH_in)
+            FH_out.filter["lowAF"] = HeaderFilterAttr("lowAF", "Variants with population AF <= {}".format(args.min_AF))
+            FH_out.filter["lowAD"] = HeaderFilterAttr("lowAD", "Variants with population AD <= {}".format(args.min_AD))
+            FH_out.filter["lowDP"] = HeaderFilterAttr("lowDP", "Variants with population DP <= {}".format(args.min_DP))
             if "ADSRC" in FH_in.format:  # Variants file come from merging of different calling
-                FH_out.filter["lowAF"] = HeaderFilterAttr("lowAF", "Variants with population AF <= {} in all variant calling sources".format(args.min_AF))
-                FH_out.filter["lowAD"] = HeaderFilterAttr("lowAD", "Variants with population AD <= {} in all variant calling sources".format(args.min_AD))
-            else:
-                FH_out.filter["lowAF"] = HeaderFilterAttr("lowAF", "Variants with population AF <= {}".format(args.min_AF))
-                FH_out.filter["lowAD"] = HeaderFilterAttr("lowAD", "Variants with population AD <= {}".format(args.min_AD))
+                for curr in ["lowAF", "lowAD", "lowDP"]:
+                    FH_out.filter[curr].description = FH_out.filter[curr].description + " in all variant calling sources"
             FH_out.writeHeader()
             # Records
             for record in FH_in:
@@ -69,11 +70,15 @@ if __name__ == "__main__":
                         new_filters.add("lowAF")
                     if max(pop_AD) < args.min_AD:
                         new_filters.add("lowAD")
+                    if max(pop_DP) < args.min_DP:
+                        new_filters.add("lowDP")
                 else:
                     if record.getPopAltAF()[0] < args.min_AF:
                         new_filters.add("lowAF")
                     if record.getPopAltAD()[0] < args.min_AD:
                         new_filters.add("lowAD")
+                    if record.getPopDP()[0] < args.min_DP:
+                        new_filters.add("lowDP")
                 if len(new_filters) > 0:
                     nb_filtered += 1
                 # Filter record
@@ -84,8 +89,8 @@ if __name__ == "__main__":
                         FH_out.write(record)
                 else:
                     old_filters = set()
-                    if record.filter is None and len(record.filter) != 0 and record.filter[0] != "PASS":
-                        record.filter = set(record.filter)
+                    if record.filter is not None and len(record.filter) != 0 and record.filter[0] != "PASS":
+                        old_filters = set(record.filter)
                     record.filter = sorted(old_filters | new_filters)
                     if len(record.filter) == 0:
                         record.filter = ["PASS"]
