5b62cf60c26b10bfef53329d5f5be04811c360d3
fescudie <escudie.frederic@iuct-oncopole.fr>
2021-01-20 16:35:29 +0100

    Update bin/evidences/annotEvidences.py for standardized evidences database.

--- bin/evidences/annotEvidences.py
+++ bin/evidences/annotEvidences.py
@@ -41,11 +41,11 @@ def getVariantCategory(variant):
     else:
         variant_category = variant.type()
         if variant_category == "variation":
-            variant_category = "mutation>mnv"
+            variant_category = "mutation>substitution"
         elif variant_category == "indel":
             variant_category = "mutation>insertion" if variant.isInsertion() else "mutation>deletion"
         else:
-            variant_category = "mutation>snv"
+            variant_category = "mutation>substitution"
     return variant_category
 
 
@@ -59,24 +59,24 @@ def loadEvidences(in_evidences):
     return evidences_by_gene_id
 
 
-def getDiseaseElt(in_diseases_ontology, disease_id, disease_term):
+def getDiseaseElt(in_disease_ontology, disease_id, disease_term):
     if disease_id is not None or disease_term is not None:
         if disease_id is None:
-            ontology = get_ontology("file://{}".format(in_diseases_ontology)).load()
+            ontology = get_ontology("file://{}".format(in_disease_ontology)).load()
             diseases = ontology.search(label=disease_term)
             if len(diseases) == 0:
-                raise Exception('The disease "{}" cannot be found in ontology {}.'.format(disease_term, in_diseases_ontology))
+                raise Exception('The disease "{}" cannot be found in ontology {}.'.format(disease_term, in_disease_ontology))
             elif len(diseases) != 1:
-                raise Exception('The disease "{}" is found several times in ontology {}.'.format(disease_term, in_diseases_ontology))
+                raise Exception('The disease "{}" is found several times in ontology {}.'.format(disease_term, in_disease_ontology))
             disease_id = str(diseases[0].id[0].split(":")[1])
         elif disease_term is None:
-            ontology = get_ontology("file://{}".format(in_diseases_ontology)).load()
+            ontology = get_ontology("file://{}".format(in_disease_ontology)).load()
             disease_term = str(ontology.search(id="DOID:{}".format(disease_id))[0].label[0])
     return disease_id, disease_term
 
 
-def getDiseaseAncestors(in_diseases_ontology, vcf_handle):
-    ontology = get_ontology("file://{}".format(in_diseases_ontology)).load()
+def getDiseaseAncestors(in_disease_ontology, vcf_handle):
+    ontology = get_ontology("file://{}".format(in_disease_ontology)).load()
     disease_ancestors_by_spl = {}
     for spl_name in vcf_handle.samples:
         spl_doid = vcf_handle.sample_info[spl_name].doid
@@ -88,24 +88,51 @@ def getDiseaseAncestors(in_diseases_ontology, vcf_handle):
     return disease_ancestors_by_spl
 
 
+def insCouldBeIdentical(hgvs_ins, hgvs_repeat):
+    # G13_L16dup vs L16_I17insGTTL
+    could_be_identical = False
+    len_dup = hgvs_repeat.end_pos - hgvs_repeat.start_pos + 1
+    if hgvs_repeat.evt is None:  # Repeat
+        len_dup = len_dup * int(hgvs_repeat.new_elts[1])
+    if len(hgvs_ins.evt.new_elts) == len_dup:  # same length
+        if hgvs_repeat.end_aa + hgvs_repeat.end_pos == hgvs_ins.start_aa + hgvs_ins.start_pos:  # Same positions
+            if hgvs_repeat.start_aa == hgvs_ins.new_elts[0] and hgvs_repeat.end_aa == hgvs_ins.new_elts[-1]:  # Same start and end amino acids
+                could_be_identical = True
+    return could_be_identical
+
+
 def getEvidences(record, annot_field):
+    variant_category = getVariantCategory(record)
     processed_associations = set()
     retained_evidences = list()
     for annot in record.info[annot_field]:
-        if annot["HGVSp"] is not None: ################# and not (annot["HGVSp"].endswith("p.?") or annot["HGVSp"].endswith("=")):  ####################
+        if annot["HGVSp"] is not None and not annot["HGVSp"].endswith("p.?") and not annot["HGVSp"].replace(")", "").endswith("="):
             annot_association_id = "{}:\t{}".format(annot["SYMBOL"], annot["HGVSp"].split(":", 1)[1])
             if annot_association_id not in processed_associations:  # Skip association on other annotation with the same impact
                 processed_associations.add(annot_association_id)
                 associations = evidences_by_gene_id[annot["Gene"]] if annot["Gene"] in evidences_by_gene_id else []
                 log.debug("{}: {} initial associations".format(record.getName(), len(associations)))
                 for curr_asso in associations:
-                    if varCatAreCompatible(variant_category, curr_asso["variant_category"]):
-                        if curr_asso["HGVSp_change"] == "":
+                    if curr_asso["HGVSp_change"] == "" and curr_asso["HGVSp_change_trunc"] == "":
+                        if varCatAreCompatible(variant_category, curr_asso["category"]):
                             retained_evidences.append(curr_asso)
-                        else:
-                            record_hgvs_p = HGVSProtChange.fromStr(annot["HGVSp"].rsplit("p.", 1)[1])
-                            record_hgvs_p.predicted = False
-                            if str(record_hgvs_p).startswith(curr_asso["HGVSp_change"]): ###############pb Val15insA et Val15insAV
+                    else:
+                        record_hgvs_p = HGVSProtChange.fromStr(annot["HGVSp"].rsplit("p.", 1)[1])
+                        record_hgvs_p.predicted = False
+                        if str(record_hgvs_p) == curr_asso["HGVSp_change"]:
+                            retained_evidences.append(curr_asso)
+                        elif curr_asso["HGVSp_change"] != "":  ################################# to test
+                            # Try to match insertions and duplications/repeats
+                            asso_hgvs_p = HGVSProtChange.fromStr(curr_asso["HGVSp_change"])
+                            if record_hgvs_p.isInFrameIns() and asso_hgvs_p.isInFrameIns():  # Are insertions
+                                if (record_hgvs_p.evt == "ins" and asso_hgvs_p.evt != "ins"):  # Record is insertion and association is repeat
+                                    if insCouldBeIdentical(record_hgvs_p, asso_hgvs_p):
+                                        retained_evidences.append(curr_asso)
+                                elif (record_hgvs_p.evt != "ins" and asso_hgvs_p.evt == "ins"):  # Record is repeat and association is insertion
+                                    if insCouldBeIdentical(asso_hgvs_p, record_hgvs_p):
+                                        retained_evidences.append(curr_asso)
+                        else:  # Truncated HGVS (ex: G12)
+                            if str(record_hgvs_p).startswith(curr_asso["HGVSp_change_trunc"]):
                                 retained_evidences.append(curr_asso)
     return retained_evidences
 
@@ -143,6 +170,33 @@ def addHeaderFields(writer, disease_id=None, disease_term=None):
             writer.sample_info[spl_name].case_by_attr["doid"] = "DOID"
 
 
+def writeEvidencesDetail(out_file, details_by_spl):
+    if not out_file.endswith(".tsv"):
+        with open(out_file, "w") as writer_evidences:
+            json.dump(details_by_spl, writer_evidences)
+    else:
+        with HashedSVIO(out_file, "w") as writer_evidences:
+            writer_evidences.titles = [
+                "sample", "variant_gene", "variant_HGVSp", "variant_genomic_alteration",
+                "evidence_subject", "evidence_disease", "evidence_level",
+                "evidence_type", "evidence_direction", "evidence_drugs",
+                "evidence_clinical_significance", "evidence_citation", "evidence_source",
+                "evidence_doid", "evidence_category", "evidence_variant_specificity",
+                "evidence_disease_specificity"
+            ]
+            for spl_name, spl in details_by_spl.items():
+                for curr_variant in spl["variants"]:
+                    for curr_evidence in curr_variant["evidences"]:
+                        curr_evidence["sample"] = spl_name
+                        for key in ["gene", "HGVSp", "genomic_alteration"]:
+                            curr_evidence["variant_" + key] = curr_variant[key]
+                        for key in [elt for elt in writer_evidences.titles if elt.startswith("evidence_") and not elt.endswith("_specificity")]:
+                            curr_evidence[key] = curr_evidence[key.replace("evidence_", "")]
+                        curr_evidence["evidence_variant_specificity"] = curr_evidence["specificity"]["variant"]
+                        curr_evidence["evidence_disease_specificity"] = curr_evidence["specificity"]["disease"]
+                        writer_evidences.write(curr_evidence)
+
+
 ########################################################################
 #
 # MAIN
@@ -150,7 +204,7 @@ def addHeaderFields(writer, disease_id=None, disease_term=None):
 ########################################################################
 if __name__ == "__main__":
     # Manage parameters
-    parser = argparse.ArgumentParser(description='Add clinical evidence level to known variants.')
+    parser = argparse.ArgumentParser(description='Add clinical/drug evidence level to known variants.')
     parser.add_argument('-a', '--annotation-field', default="ANN", help='Field used for store annotations. [Default: %(default)s]')
     group_disease = parser.add_mutually_exclusive_group()
     group_disease.add_argument('-d', '--disease-id', help='Replace samples disease by this value. It must be a valid ID in disease ontology.')
@@ -159,10 +213,10 @@ if __name__ == "__main__":
     group_input = parser.add_argument_group('Inputs')
     group_input.add_argument('-e', '--input-evidences', required=True, help='Path to the clinical evidence file from AnaCore-utils/bin/enrichCivic.py (format: TSV).')
     group_input.add_argument('-i', '--input-variants', required=True, help='Path to the variants file. (format: VCF).')
-    group_input.add_argument('-n', '--input-diseases-ontology', required=True, help='Path to the diseases ontology file. (format: OWL).')
+    group_input.add_argument('-n', '--input-disease-ontology', required=True, help='Path to the disease ontology file. (format: OWL).')
     group_output = parser.add_argument_group('Outputs')
     group_output.add_argument('-o', '--output-variants', required=True, help='Path to the annotated file. (format: VCF).')
-    group_output.add_argument('-s', '--output-evidences', required=True, help='Path to the evidences associated with variants. (format: TSV).')
+    group_output.add_argument('-s', '--output-evidences', help='Path to the evidences associated with variants. (format: TSV or JSON depends on extension).')
     args = parser.parse_args()
 
     # Logger
@@ -172,9 +226,8 @@ if __name__ == "__main__":
     log.info("Command: " + " ".join(sys.argv))
 
     # Process
-    nb_unknown_significance = 0
-    nb_with_association = 0
-    disease_id, disease_term = getDiseaseElt(args.input_diseases_ontology, args.disease_id, args.disease_term)
+    nb_variants = {"total": 0, "with_asso": 0}
+    disease_id, disease_term = getDiseaseElt(args.input_disease_ontology, args.disease_id, args.disease_term)
     evidences_by_gene_id = loadEvidences(args.input_evidences)
     details_by_spl = {}
     with AnnotVCFIO(args.output_variants, "w") as writer:
@@ -183,7 +236,7 @@ if __name__ == "__main__":
             writer.copyHeader(reader)
             addHeaderFields(writer, disease_id, disease_term)
             writer.writeHeader()
-            disease_ancestors_by_spl = getDiseaseAncestors(args.input_diseases_ontology, writer)
+            disease_ancestors_by_spl = getDiseaseAncestors(args.input_disease_ontology, writer)
             for spl_name, spl in writer.sample_info.items():
                 details_by_spl[spl.id] = {
                     "variants": [],
@@ -197,8 +250,8 @@ if __name__ == "__main__":
                         sorted(set(annot["SYMBOL"] for annot in record.info[args.annotation_field]))
                     )
                 )
+                nb_variants["total"] += 1
                 # Get corresponding clinical evidences
-                variant_category = getVariantCategory(record)
                 retained_evidences = getEvidences(record, args.annotation_field)
                 # Select best evidences
                 best_evidence = {}
@@ -216,22 +269,20 @@ if __name__ == "__main__":
                         "evidences": []
                     })
                 if len(retained_evidences) != 0:
-                    nb_with_association += 1
-                    for curr_evidence in sorted(retained_evidences, key=lambda elt: elt["evidence_level"]):
+                    nb_variants["with_asso"] += 1
+                    for curr_evidence in sorted(retained_evidences, key=lambda elt: elt["level"]):
                         detailed_annot = {
-                            "subject": curr_evidence["cleaned_variant"],
-                            "subject_category": curr_evidence["variant_category"],
+                            "subject": curr_evidence["subject"],
+                            "category": curr_evidence["category"],
                             "disease": curr_evidence['disease'],
                             "doid": curr_evidence["doid"],
-                            "level": curr_evidence['evidence_level'],
-                            "status": curr_evidence['evidence_status'],
-                            "type": curr_evidence['evidence_type'],
-                            "direction": curr_evidence['evidence_direction'],
+                            "level": curr_evidence['level'],
+                            "type": curr_evidence['type'],
+                            "direction": curr_evidence['direction'],
                             "drugs": curr_evidence['drugs'],
-                            "drug_interaction_type": curr_evidence['drug_interaction_type'],
                             "clinical_significance": curr_evidence['clinical_significance'],
-                            "citation": "{}:{}".format(curr_evidence['source_type'], curr_evidence['citation_id']),
-                            "source": "CIViC",
+                            "citation": curr_evidence['citation'],
+                            "source": curr_evidence['source'],
                             "specificity": {"variant": "imprecise", "disease": "all"}
                         }
                         for spl_name in record.samples:
@@ -239,7 +290,7 @@ if __name__ == "__main__":
                             evidence_for_spl = copy.deepcopy(detailed_annot)
                             # Best evidence
                             variant_specificity = ["i"]
-                            if curr_evidence["HGVSp_change"] != "":  ########################
+                            if curr_evidence["HGVSp_change"] != "":
                                 variant_specificity = ["p", "i"]
                                 evidence_for_spl["specificity"]["variant"] = "precise"
                             disease_specificity = ["a"]
@@ -250,12 +301,10 @@ if __name__ == "__main__":
                                 for curr_d_spe in disease_specificity:
                                     best_tag = "{}{}".format(curr_v_spe, curr_d_spe)
                                     prev = best_evidence[spl_name][best_tag]
-                                    if prev == "" or prev > curr_evidence['evidence_level']:
-                                        best_evidence[spl_name][best_tag] = curr_evidence['evidence_level']
+                                    if prev == "" or prev > curr_evidence['level']:
+                                        best_evidence[spl_name][best_tag] = curr_evidence['level']
                             # Add to detail
                             details_by_spl[spl_name]["variants"][-1]["evidences"].append(evidence_for_spl)
-                else:
-                    nb_unknown_significance += 1
                 # Add best evidence to record
                 for spl_name in record.samples:
                     spl_disease = writer.sample_info[spl_name].doid
@@ -267,27 +316,15 @@ if __name__ == "__main__":
                 record.format.extend(["EVID_PS", "EVID_PA", "EVID_IS", "EVID_IA"])
                 # Write record
                 writer.write(record)
-    #
-    # with HashedSVIO(args.output_evidences, "w") as writer_evidences:
-    #     writer_evidences.titles = [
-    #         "sample", "variant_gene", "variant_hgvsp", "variant_genomic_alteration",
-    #         "evidence_variant", "evidence_disease", "evidence_level", "evidence_status",
-    #         "evidence_type", "evidence_direction", "drugs", "drug_interaction_type",
-    #         "clinical_significance", "citation", "source", "evidence_doid",
-    #         "evidence_variant_category", "evidence_variant_specificity",
-    #         "evidence_disease_specificity"
-    #     ]
-    #     for spl_name, spl in details_by_spl.items():
-    #         for curr_evidence in spl["evidences"]:
-    #             curr_evidence["sample"] = spl_name
-    #             writer_evidences.write(curr_evidence)
-    with open(args.output_evidences, "w") as writer_evidences:
-        json.dump(details_by_spl, writer_evidences)
 
+    # Write outputs
+    if args.output_evidences:
+        writeEvidencesDetail(args.output_evidences, details_by_spl)
 
+    # Log
     log.info("{:.2%} of variants have an association ({}/{})".format(
-        nb_with_association / (nb_with_association + nb_unknown_significance),
-        nb_with_association,
-        nb_with_association + nb_unknown_significance
+        0 if nb_variants["total"] == 0 else nb_variants["with_asso"] / nb_variants["total"],
+        nb_variants["with_asso"],
+        nb_variants["total"]
     ))
     log.info("End of job")
