053d40c55a3898f28b957f39f8efb8b4a5c70bdc
fescudie <escudie.frederic@iuct-oncopole.fr>
2026-07-06 16:45:12 +0200

    Use pyproject.toml instead of deprecated setup.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import re
-from distutils.core import setup
+from setuptools import setup
 
 
 def get_version():
@@ -14,36 +14,6 @@ def get_version():
     return version
 
 
-def load_scripts(path):
-    scripts = []
-    for filename in os.listdir(path):
-        filepath = os.path.join(path, filename)
-        if os.path.isdir(filepath):
-            if filename != "test":
-                load_scripts(filepath)
-        else:
-            if filename.endswith(".py") and not filename.startswith("__"):
-                scripts.append(filepath)
-    return scripts
-
-
-def load_requirements(path):
-    requirements = []
-    with open(path) as FH:
-        requirements = [elt.strip().replace(" ", "") for elt in FH]
-    return requirements
-
-
 setup(
-    name='anacore-utils',
     version=get_version(),
-    description='Scripts for easily process NGS data.',
-    long_description='Scripts for easily process NGS data from medical centers. This package contains several aggregators, converters, filters, wrappers, etc.',
-    author='Frederic Escudie',
-    author_email='escudie.frederic@iuct-oncopole.fr',
-    license='GNU GPL v3',
-    packages=["bin"],
-    python_requires='>=3.10',
-    install_requires=load_requirements("requirements.txt"),
-    scripts=load_scripts("bin")
 )
